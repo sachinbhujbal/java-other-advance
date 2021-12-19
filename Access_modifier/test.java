e27f385eec701d60a3a477e48304483f251b1cf0
Sachin Prakash Bhujbal <87747541+sachinbhujbal@users.noreply.github.com>
2021-12-20 00:47:33 +0530

    Update test.java
    using the variable initilization in the private mehtod as an arugment variable 's'.
    So, this are the method access modifiers.

--- Access_modifier/test.java
+++ Access_modifier/test.java
@@ -1,10 +1,19 @@
 package Access_modifier;
-
+public void display(){
+	System.out.println("This is display method");
+}
+private void display2(int s){
+	System.out.println("This is display2 method"+s);
+}
+protected void nothing(){
+	System.out.println("this method return nothing");
+}
+		
 public class test {
  public static void main(String[] args) {
 	A a = new A();
 	a.display();
-	a.display2();
+	a.display2(34);
 	a.nothing();
 }
 }

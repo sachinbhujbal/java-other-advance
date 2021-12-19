516b880f8c109712b14cba089ed7913adfad8b82
Sachin Prakash Bhujbal <87747541+sachinbhujbal@users.noreply.github.com>
2021-12-20 00:50:15 +0530

    Update B.java
    Using the arrgument variable at private method for more clearification.
    and by creating the methods as a access modifiers.

--- Access_modifier/B.java
+++ Access_modifier/B.java
@@ -1,5 +1,20 @@
 package Access_modifier;
 
-public class B {
-
+public void display(){
+	System.out.println("This is display method");
+}
+public void display2(int s){
+	System.out.println("This is display2 method"+s);
+}
+protected void nothing(){
+	System.out.println("this method return nothing");
+}
+		
+public class test {
+ public static void main(String[] args) {
+	A a = new A();
+	a.display();
+	a.display2(34);
+	a.nothing();
+}
 }
